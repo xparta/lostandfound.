c82b907564ba35ef7d8dc52c95e12854c8405c08
xparta <tomarobaski@gmail.com>
2013-11-19 14:21:34 +0300

    added home about

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,3 @@
+def about
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 LostFound::Application.routes.draw do
-  get "pages/home"
+  # root "home" => "pages#home"
+  get "home" => "pages#home"
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
